01064e6918da8adb7de54cd3c1abba92ebc491c4
niels <niels@001aeff7-3401-0410-a489-f7902fc005dd>
2012-11-07 09:56:59 +0000

    git-svn-id: https://svn.tribler.org/abc/branches/release-6.0.x-GOLF@29437 001aeff7-3401-0410-a489-f7902fc005dd

--- Tribler/community/gossiplearningframework/models/p2pegasos.py
+++ Tribler/community/gossiplearningframework/models/p2pegasos.py
@@ -1,18 +1,8 @@
-from models import GossipLearningModel
 import numpy as np
-if __debug__:
-    from Tribler.dispersy.dprint import dprint
+from models import GossipLearningModel
 
-def safe_sparse_dot(a, b, dense_output=False):
-    """Dot product that handle the sparse matrix case correctly"""
-    from scipy import sparse
-    if sparse.issparse(a) or sparse.issparse(b):
-        ret = a * b
-        if dense_output and hasattr(ret, "toarray"):
-            ret = ret.toarray()
-        return ret
-    else:
-        return np.dot(a, b)
+if __debug__:
+    from Tribler.Core.dispersy.dprint import dprint
 
 class P2PegasosModel(GossipLearningModel):
 
@@ -20,40 +10,32 @@ class P2PegasosModel(GossipLearningModel):
         super(P2PegasosModel, self).__init__()
 
         # Initial model
-        self.w = [0]
+        self.w = np.array([0])
         self.age = 0
 
     def update(self, x, y):
         """Update the model with a new training example."""
         # Set up some variables.
         label = -1.0 if y == 0 else 1.0
-
+        
         self.age = self.age + 1
         lam = 0.0001
         rate = 1.0 / (self.age * lam)
 
-        import sys
-        print >> sys.stderr, "w", self.w
-        print >> sys.stderr, "x", x
-
-        is_sv = label * np.sum(safe_sparse_dot(self.w, x)) < 1.0
-        for i in range(len(self.w)):
-            if is_sv:
-                self.w[i] = (1.0 - 1.0 / self.age) * self.w[i] + rate * label * x[i]
-            else:
-                self.w[i] = (1.0 - 1.0 / self.age) * self.w[i]
-                
-        print >> sys.stderr, "after_w", self.w
+        is_sv = label * sum(self.w * x) < 1.0
+        self.w = self.w * (1.0 - 1.0 / self.age)
+        if is_sv:
+            self.w = self.w + (rate * label * x)
 
     def predict(self, x):
         """
         Compute the inner product of the hyperplane and the instance as a
         prediction.
         """
-        wx = np.sum(safe_sparse_dot(self.w, x))
+        wx = sum(self.w * x)
         return 1.0 if wx >= 0.0 else 0.0
 
     def merge(self, model):
         self.age = max(self.age, model.age)
-        self.w = [(self.w[i] + model.w[i]) / 2.0 for i in range(len(self.w))]
+        self.w = (self.w + model.w) / 2.0
 
